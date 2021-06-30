41b39db0179ed10df9c75942f2a62360575a30b5
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-06-30 14:59:21 +0300

    updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 // import { Typography } from '@material-ui/core';
 // import SignInSide from './components/signIn/SignInSide';
-// import Dashboard from './components/dashboard/Dashboard';
-// import Header from './Components/header/Header';
 // import LandingPage from './pages/landingPage';
-import Overview from './pages/overview'
+import Overview from './pages/overview';
+// import Suppliers from './pages/suppliers';
+// import Loaders from './pages/loaders';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      {/* <SignInSide /> */}
-      {/* <Dashboard /> */}
-      {/* <LandingPage/> */}
-      {/* <Header /> */}
       <Overview />
     </div>
   );
--- src/pages/addSuppliers.tsx
+++ src/pages/addSuppliers.tsx
@@ -1,12 +1,96 @@
 import React from 'react'
-import Dashboard from '../components/dashboard/Dashboard';
 
-function AddSuppliers() {
+function addSuppliers() {
   return (
     <div>
-      <Dashboard />
+      
     </div>
   )
 }
 
-export default AddSuppliers
+export default addSuppliers
+
+
+
+// import React from 'react';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+
+// const AddSupplier = () => {
+//   const [open, setOpen] = React.useState(false);
+
+//   const handleClickOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+//   return (
+//     <div>
+//       {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+//         Open form dialog
+//       </Button> */}
+//       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+//         <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+//         <DialogContent>
+
+//           <DialogContentText>
+//             Add Supplier
+//           </DialogContentText>
+
+//           <TextField
+//             autoFocus
+//             margin='dense'
+//             id='name'
+//             label='Name of Supplier'
+//             fullWidth
+//           />
+
+//           <TextField
+//             autoFocus
+//             margin='dense'
+//             id='address'
+//             label=' Addres of Supplier'
+//             fullWidth
+//           />
+
+//           <TextField
+//             autoFocus
+//             margin='dense'
+//             id='contact'
+//             label='Contact/ Phone/ Email'
+//             fullWidth
+//           />
+
+//           <TextField
+//             autoFocus
+//             margin='dense'
+//             id='More details'
+//             label='More Details'
+//             fullWidth
+//           />
+
+
+
+//         </DialogContent>
+//         <DialogActions>
+//           <Button onClick={handleClose} color="primary">
+//             Cancel
+//           </Button>
+//           <Button onClick={handleClose} color="primary">
+//             Add Suplier
+//           </Button>
+//         </DialogActions>
+//       </Dialog>
+//     </div>
+//   );
+// }
+
+// export default AddSupplier;
