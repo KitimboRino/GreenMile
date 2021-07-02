2f1608d7824f5e20a699162e74dcef935cbf305c
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-07-02 15:23:10 +0300

    Updated code

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import logo from '../logo/gmLogo.png';
 import Button from '@material-ui/core/Button';
 
